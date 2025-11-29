d5a5a935ef81c22f03b6fbd28a21ef03e88f2c23
ealejandrog <ealejandrog1@gmail.com>
2025-11-28 21:45:41 -0600

    se arregla seeder de pokemones

--- utils/pokemons.ts
+++ utils/pokemons.ts
@@ -1,31 +1,30 @@
 import { Pokemon } from "@/interfaces/Pokemon.interface";
-import { IMAGES } from "./get-images";
 
 
 export const POKEMONS: Pokemon[] = [
     {
         id: 1,
         name: "Bulbasaur",
-        image: '/1.png',
+        imageUrl: '/1.png',
     },
     {
         id: 2,
         name: "Ivysaur",
-        image: '/2.png',
+        imageUrl: '/2.png',
     },
     {
         id: 3,
         name: "Venusaur",
-        image: '/3.png',
+        imageUrl: '/3.png',
     },
     {
         id: 4,
         name: "Charmander",
-        image: '/4.png',
+        imageUrl: '/4.png',
     },
     {
         id: 5,
         name: "Charmeleon",
-        image: '/5.png',
+        imageUrl: '/5.png',
     },
 ]
